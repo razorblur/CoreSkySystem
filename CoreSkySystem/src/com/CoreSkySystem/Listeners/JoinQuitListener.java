fbc9a0778a904c729ac7f6e2860aa1fc6596e717
Nicolai Helldorff <razorblurhd@gmail.com>
2014-09-16 17:16:08 +0200

    Join bug fixed When a player tries to join and he has too many warnings then he cant connect to the server.

--- CoreSkySystem/src/com/CoreSkySystem/Listeners/JoinQuitListener.java
+++ CoreSkySystem/src/com/CoreSkySystem/Listeners/JoinQuitListener.java
@@ -1,9 +1,10 @@
 package com.CoreSkySystem.Listeners;
 
-import org.bukkit.command.defaults.KickCommand;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerLoginEvent.Result;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 import com.CoreSkySystem.Commands.COMMAND_Warn;
@@ -13,14 +14,17 @@ public class JoinQuitListener implements Listener{
 	/* This class block the join and leave messages.*/
 	@EventHandler
 	public void onJoin(PlayerJoinEvent evt)  {
-		if(COMMAND_Warn.getWarnNumber(evt.getPlayer()) >= COMMAND_Warn.limit) {
-			evt.getPlayer().kickPlayer(COMMAND_Warn.kickMessage);
-		}
 		evt.setJoinMessage("");
 	}
 	@EventHandler
 	public void onQuit(PlayerQuitEvent evt)  {
 		evt.setQuitMessage("");
 	}
-
+	@EventHandler
+	public void onLogin(PlayerLoginEvent evt) {
+		if(COMMAND_Warn.getWarnNumber(evt.getPlayer()) >= COMMAND_Warn.limit) {
+			evt.disallow(Result.KICK_BANNED, COMMAND_Warn.kickMessage);
+		}
+	}
+	
 }

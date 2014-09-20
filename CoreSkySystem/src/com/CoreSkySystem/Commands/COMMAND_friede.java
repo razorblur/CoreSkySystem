db2bf8a1bff4c3b65c75fa9eb5aeb1af45dd6c54
Nicolai Helldorff <razorblurhd@gmail.com>
2014-09-20 22:37:02 +0200

    Edited the Command /friede

--- CoreSkySystem/src/com/CoreSkySystem/Commands/COMMAND_friede.java
+++ CoreSkySystem/src/com/CoreSkySystem/Commands/COMMAND_friede.java
@@ -1,7 +1,14 @@
 package com.CoreSkySystem.Commands;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map.Entry;
+
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -13,8 +20,8 @@ import com.CoreSkySystem.Main.Main;
 public class COMMAND_friede implements CommandExecutor {
 	// Variablen
 	public static HashMap<String, String> friede = new HashMap<>();
-	private File file = new File("plugins//" + Main.path + "//friede.yml");
-	private YamlConfiguration cfg = YamlConfiguration.loadConfiguration(file);
+	public static File file = new File("plugins//" + Main.path + "//friede.yml");
+	public static YamlConfiguration cfg = YamlConfiguration.loadConfiguration(file);
                  //spieler angebot  Spieler angebot
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String arg2,
@@ -31,13 +38,78 @@ public class COMMAND_friede implements CommandExecutor {
 			if(args.length == 1) {
 				switch(args[0].toLowerCase()) {
 				case "annehmen":
+					// player2
+					if(friede.containsValue(player.getName())) {
+						String target = getKey(player.getName());
+						ArrayList<String> player_frieden = getFriedenListe(player.getName());
+						ArrayList<String> target_frieden = getFriedenListe(target);
+						target_frieden.add(player.getName());
+						player_frieden.add(target);
+						// Set Array
+						cfg.set(player.getName(), player_frieden);
+						cfg.set(target, target_frieden);
+						try {
+							cfg.save(file);
+						} catch (IOException e) {
+							System.out.println("Datei konnte nicht gefunden werden");
+						}
+						
+						player.sendMessage(Main.name + "§7Du hast mit " + target + " frieden geschlossen");
+						Bukkit.getPlayer(target).sendMessage(Main.name + "§7Du hast mit " + player.getName() + " frieden geschlossen");
+						
+						friede.remove(target);
+					} else {
+						player.sendMessage(Main.name + "§7Keine ausstehenden Friedens Angebote");
+					}
 					break;
-				case "ablehnen": 
+				case "ablehnen":
+					if(friede.containsValue(player.getName())) {
+						String target = getKey(player.getName());
+						
+						player.sendMessage(Main.name + "§7Du hast " + target + "'s Friedensangebot abgelehnt");
+						Bukkit.getPlayer(target).sendMessage(Main.name + "§c" + player.getName() + " hat dein Friedensangebot abgelehnt");
+						
+						friede.remove(target);
+					} else {
+						player.sendMessage(Main.name + "§7Keine ausstehenden Friedens Angebote");
+					}
 					break;
 				case "list": 
 				default:
+					try {
+						Player target = Bukkit.getPlayer(args[0]);
+						// Prüfen ob die Person mit sich selbst frieden schließen möchte
+						if(target.getName().equals(player.getName())) {
+							player.sendMessage(Main.name + "§7Du kannst nicht mit dir selbst frieden schliessen");
+							return true;
+						}
+						// Prüfen ob man bereits mit der Person frieden hat
+						if(cfg.getList(player.getName()) != null) {
+							List<?> list = cfg.getList(player.getName());
+							for(int i = 0; i < list.size(); i++) {
+								if(list.get(i).equals(target.getName())) {
+									player.sendMessage(Main.name + "§7Du hast bereits mit dieser Person frieden");
+									return true;
+								}
+							}
+						}
+						
+						if(!friede.containsKey(player.getName())) {
+							friede.put(player.getName(), target.getName());
+							// Messages
+							player.sendMessage(Main.name + "§7Du hast §c" + target.getName() + " §7 ein Friedens-Angebot gestellt");
+							target.sendMessage(Main.name + "§c" + player.getName() + " §7 hat dir Friedens-Angebot gestellt");
+						} else {
+							player.sendMessage(Main.name + "§7Es gibt noch ein austehendes Friedensangebot von dir");
+						}
+
+					} catch (NullPointerException exception) { 
+						player.sendMessage(Main.name + "§4Dieser Spieler ist offline");
+					}
 					
-				} // tester razorblur
+				} // player1 player2
+			} else {
+				player.sendMessage(Main.name + "/friede (player)");
 			}
 			
 		} else {
@@ -45,6 +117,25 @@ public class COMMAND_friede implements CommandExecutor {
 		}
 		
 		return true;
+	}
+	 // player1 player2
+	private ArrayList<String> getFriedenListe(String player) {
+		ArrayList<String> players = new ArrayList<>();
+		if(cfg.getList(player) == null) return players;
+		List<?> list = (List<?>) cfg.getList(player);
+		for(int i = 0; i < list.size(); i++) {
+			players.add((String) list.get(i));
+		}
+		return players;
+	}
+	
+	private String getKey(String value) {
+		Iterator<Entry<String, String>> iterator = friede.entrySet().iterator();
+		while(iterator.hasNext()) {
+			Entry<String, String> str = iterator.next();
+			if(str.getValue() == value) return str.getKey();
+		}
+		return null;
 	}
 
 }
--- CoreSkySystem/src/com/CoreSkySystem/Listeners/PlayerListeners.java
+++ CoreSkySystem/src/com/CoreSkySystem/Listeners/PlayerListeners.java
@@ -1,11 +1,15 @@
 package com.CoreSkySystem.Listeners;
 
+import java.util.List;
+
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
+import com.CoreSkySystem.Commands.COMMAND_friede;
 import com.CoreSkySystem.Commands.COMMAND_globalmute;
 import com.CoreSkySystem.Functions.Permission;
 import com.CoreSkySystem.Main.Main;
@@ -30,6 +34,25 @@ public class PlayerListeners implements Listener {
 			player.getKiller().sendMessage(Main.name + " §8Du hast §6§l" + player.getName() + " §8getötet");
 			player.getKiller().setHealth(20.0);
 			player.getKiller().sendMessage(Main.name + " §7Du wurdest §cgeheilt §7weil du jemanden §cgetötet §7hast");
+		}
+	}
+	
+	////// Friede Listener ////////
+	@EventHandler
+	public void onDmg(EntityDamageByEntityEvent event) {
+		if(event.getEntity() instanceof Player && event.getDamager() instanceof Player) {
+			try {
+				Player player = (Player) event.getEntity();
+				Player damager = (Player) event.getDamager();
+				
+				List<?> list = COMMAND_friede.cfg.getList(damager.getName());
+				for(int i = 0; i < list.size(); i++) {
+					if(list.get(i).equals(player.getName())) {
+						event.setCancelled(true);
+						return;
+					}
+				}
+			} catch(NullPointerException exception) {}
 		}
 	}
 
